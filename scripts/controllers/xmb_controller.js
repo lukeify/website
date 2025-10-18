bc1909029d083d1071adb56252d9144edbeefb71
lukeify <5379845+lukeify@users.noreply.github.com>
2025-10-18 15:27:30 +1300

    feat: x axis navigation

---
+++ scripts/controllers/xmb_controller.js
@@ -0,0 +1,82 @@
+import {Controller} from "@hotwired/stimulus";
+
+export default class extends Controller {
+    static targets = ['primary'];
+
+    primaryTargetDataAttribute = 'data-xmb-primary-active';
+
+    get previousPrimaryTarget() {
+        return this.activePrimaryTarget.previousElementSibling;
+    }
+
+    get nextPrimaryTarget() {
+        return this.activePrimaryTarget.nextElementSibling;
+    }
+
+    get activePrimaryTarget() {
+        return this.primaryTargets.find(pt => pt.hasAttribute(this.primaryTargetDataAttribute));
+    }
+
+    handleKeydown(event) {
+        switch (event.key) {
+            case 'ArrowUp':
+                this.#zPositive();
+                break;
+            case 'ArrowDown':
+                this.#zNegative();
+                break;
+            case 'ArrowRight':
+                this.#xPositive();
+                break;
+            case 'ArrowLeft':
+                this.#xNegative();
+                break;
+        }
+    }
+
+    #xPositive() {
+        const nextTarget = this.nextPrimaryTarget;
+        if (!nextTarget) {
+            return;
+        }
+
+        this.#setNewPrimaryTarget(nextTarget);
+        this.#setTranslation('x', -this.previousPrimaryTarget.getBoundingClientRect().width);
+    }
+
+    #xNegative() {
+        const prevTarget = this.previousPrimaryTarget;
+        if (!prevTarget) {
+            return;
+        }
+
+        this.#setTranslation('x', this.previousPrimaryTarget.getBoundingClientRect().width);
+        this.#setNewPrimaryTarget(prevTarget);
+    }
+
+    #zPositive() {
+
+    }
+
+    #zNegative() {
+
+    }
+
+    #currentTranslation() {
+        const computedStyle = window.getComputedStyle(this.element);
+        return {
+            x: parseFloat(computedStyle.getPropertyValue('--x-translation')),
+            z: parseFloat(computedStyle.getPropertyValue('--z-translation'))
+        };
+    }
+
+    #setTranslation(axis, adjustment) {
+        const newValue = this.#currentTranslation()[axis] + adjustment;
+        this.element.style.setProperty(`--${axis}-translation`, `${newValue}px`);
+    }
+
+    #setNewPrimaryTarget(newTarget) {
+        this.activePrimaryTarget.removeAttribute(this.primaryTargetDataAttribute);
+        newTarget.setAttribute(this.primaryTargetDataAttribute, '');
+    }
+}
